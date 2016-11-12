6716b6bba7fc7fc501b18a6a09b8b0bf9d4c5f2e
Steven Locke <s3locke@uwaterloo.ca>
2016-11-11 18:46:26 -0800

    WIP. Don't push this.

--- src/js/pages/organizer/Branding.jsx
+++ src/js/pages/organizer/Branding.jsx
@@ -1,9 +1,41 @@
 import React from "react";
 
 export default class Branding extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    	name: '',
+    	organization: '',
+    	dateStart: '',
+    	dateEnd: '',
+    	colourPrimary: '',
+    	colourSecondary: '',
+    	logo: '',
+    	logoSquare: '',
+    	icon: '',
+    };
 
-  render() {
-  	return (<div> Branding </div>);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  handleChange(event) {
+    this.setState({name: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.state);
+  }
+
+  render() {
+  	debugger
+    return (
+      <form onSubmit={this.handleSubmit}>
+        Name: <input type="text" value={this.state.name} onChange={this.handleChange} />
+        Organization: <input type="text" value={this.state.organization} onChange={this.handleChange} />
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
 }
