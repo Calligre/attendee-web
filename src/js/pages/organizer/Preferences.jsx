c86fbf9824d6ab04f96943651bf56a6780f8dfc1
Lara Janecka <numbersandlettersandallthat@gmail.com>
2016-11-21 22:11:14 -0800

    Add basic functions on preference page

--- src/js/pages/organizer/Preferences.jsx
+++ src/js/pages/organizer/Preferences.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React from 'react';
 
 export default class Preferences extends React.Component {
-
-  render() {
-  	return (<div> Preferences </div>);
+  constructor(props) {
+    super(props);
+    this.state = {
+      newsfeed: false,
+      cards: false,
+      info: false,
+      facebook: false,
+      twitter: false,
+      reposts: false,
+    };
   }
 
+  componentWillMount() {
+    PreferenceStored.on('received', this.getPeople);
+    PreferenceStore.on('error', this.showError);
+  }
+
+  componentWillUnmount() {
+    PreferenceStored.removeListener('received', this.getPeople);
+    PreferenceStore.removeListener('error', this.showError);
+  }
+
+  getPeople = () => {
+    this.setState(PreferenceStore.preferences);
+  }
+
+  showError() {
+    console.log(PreferenceStore.error);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  render() {
+    return (
+      <div> Preferences </div>
+
+    );
+  }
 }
