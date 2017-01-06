9628d93ecfc53b8e8eb12fd64f7705cbb48dccb3
Lara Janecka <numbersandlettersandallthat@gmail.com>
2017-01-06 18:29:02 -0500

    Added checkboxes

--- src/js/pages/organizer/Preferences.jsx
+++ src/js/pages/organizer/Preferences.jsx
@@ -4,6 +4,7 @@ export default class Preferences extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      activeTab: 'pages',
       newsfeed: false,
       cards: false,
       info: false,
@@ -14,31 +15,144 @@ export default class Preferences extends React.Component {
   }
 
   componentWillMount() {
-    PreferenceStored.on('received', this.getPeople);
-    PreferenceStore.on('error', this.showError);
+    // PreferenceStored.on('received', this.getPeople);
+    // PreferenceStore.on('error', this.showError);
   }
 
   componentWillUnmount() {
-    PreferenceStored.removeListener('received', this.getPeople);
-    PreferenceStore.removeListener('error', this.showError);
+    // PreferenceStored.removeastener('received', this.getPeople);
+    // PreferenceStore.removeListener('error', this.showError);
   }
 
   getPeople = () => {
-    this.setState(PreferenceStore.preferences);
+    // this.setState(PreferenceStore.preferences);
   }
 
-  showError() {
-    console.log(PreferenceStore.error);
+  showError () {
+    // console.log(PreferenceStore.error);
   }
 
-  handleChange(event) {
+  handleChange = (event) => {
     this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleCheckboxChange = (event) => {
+    this.setState({ [event.target.name]: event.target.checked });
+  }
+
+  handleTabSwitch = (event) => {
+    this.setState({ activeTab: event.target.id });
+  }
+
+  render() {
+    const { activeTab } = this.state;
+
+    let tab = null;
+    switch (activeTab) {
+      case 'pages':
+        tab = <TabPages {...this.state} handleChange={this.handleCheckboxChange} />;
+        break;
+      case 'social-media':
+        tab = <TabSocialMedia {...this.state} handleChange={this.handleCheckboxChange} />;
+        break;
+      case 'surveys':
+        tab = <TabSurveys/>;
+        break;
+      case 'other':
+        tab = <TabOther/>;
+        break;
+      default:
+        null;
+        break;
+    }
+
+    return (
+      <div>
+        <div className="sidebar">
+          <div id="pages" onClick={this.handleTabSwitch}>Pages</div>
+          <div id="social-media" onClick={this.handleTabSwitch}>Social Media</div>
+          <div id="surveys" onClick={this.handleTabSwitch}>Surveys</div>
+          <div id="other" onClick={this.handleTabSwitch}>Other</div>
+        </div>
+        {tab}
+      </div>
+    );
+  }
+}
+
+class TabPages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = props;
   }
 
   render() {
     return (
-      <div> Preferences </div>
-
+      <div>
+        <div>
+          <label>Newsfeed
+            <input type="checkbox"
+              name="newsFeed"
+              onChange={this.props.handleChange}
+              value={this.state.newsfeed}
+            />
+          </label>
+        </div>
+      </div>
     );
   }
 }
+
+class TabSocialMedia extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = props;
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <label>Faceboox
+            <input type="checkbox"
+              name="facebook"
+              onChange={this.props.handleChange}
+              checked={this.state.facebook}
+            />
+          </label>
+        </div>
+        <div>
+          <label>Twitter
+            <input type="checkbox"
+              name="twitter"
+              onChange={this.props.handleChange}
+              checked={this.state.twitter}
+            />
+          </label>
+        </div>
+        <div>
+          <label>Reposts
+            <input type="checkbox"
+              name="reposts"
+              onChange={this.props.handleChange}
+              checked={this.state.reposts}
+            />
+          </label>
+        </div>
+
+      </div>
+    );
+  }
+};
+
+const TabSurveys = React.createClass({
+  render() {
+    <div>Surveys</div>
+  }
+});
+
+const TabOther = React.createClass({
+  render() {
+    <div>Other</div>
+  }
+});
