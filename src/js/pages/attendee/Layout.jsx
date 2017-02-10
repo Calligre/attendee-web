0a17af8019b63a25f019c8e59623310d39328ad9
Lara Janecka <numbersandlettersandallthat@gmail.com>
2017-02-10 17:27:21 -0500

    swith to stylesheets

--- src/js/pages/attendee/Layout.jsx
+++ src/js/pages/attendee/Layout.jsx
@@ -7,27 +7,19 @@ import Nav from "components/layout/attendee/Nav";
 export default class Layout extends React.Component {
   render() {
     const { location } = this.props;
-    const containerStyle = {
-      marginTop: "60px"
-    };
 
     return (
       <div>
-
         <Nav location={location}/>
-
-        <div class="container" style={containerStyle}>
+        <div class="container navHeader">
           <div class="row">
             <div class="col-lg-12">
-
               {this.props.children}
-
             </div>
           </div>
           <Footer/>
         </div>
       </div>
-
     );
   }
 }
