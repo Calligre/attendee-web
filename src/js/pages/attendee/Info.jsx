dd35f60175142a75103131bcd76379e8b096f280
Steven Locke <s3locke@uwaterloo.ca>
2017-03-08 16:40:50 -0500

    Updated the Info page to properly use AuthHeaders
    And successfully retrieve the information.
    Also linted.

--- src/js/pages/attendee/Info.jsx
+++ src/js/pages/attendee/Info.jsx
@@ -1,57 +1,64 @@
-import React from "react";
+import AuthService from 'util/AuthService';
+import React from 'react';
 
-var moment = require('moment');
-var $ = require('jquery');
+const moment = require('moment');
+const $ = require('jquery');
 
 export default class Info extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      logo: "/splash",
-      confName: "Loading...",
-      location: "Loading...",
+      logo: '',
+      conf_name: 'Loading...',
+      location: 'Loading...',
       startDate: moment().unix(),
       endDate: moment().unix(),
-      other: "Loading...",
-      twitter: "Loading...",
-      facebook: "Loading...",
+      other: 'Loading...',
+      twitter: 'Loading...',
+      facebook: 'Loading...',
     };
   }
 
   componentDidMount() {
-    this.serverRequest = $.get(`${this.props.route.apiBaseURL}/info`, (result) => {
-      this.setState({
-        logo: result.data.attributes.logo,
-        confName: result.data.attributes.name,
-        location: result.data.attributes.location,
-        starttime: result.data.attributes.starttime,
-        endtime: result.data.attributes.endtime,
-        other: result.data.attributes.other,
-        twitter: result.data.attributes.twitter,
-        facebook: result.data.attributes.facebook,
-      });
+    const self = this;
+    $.ajax({
+      url: `${this.props.route.apiBaseURL}/info`,
+      dataType: 'json',
+      type: 'GET',
+      headers: {
+        Authorization: `Bearer ${AuthService.getToken()}`,
+      },
+
+      cache: true,
+      success(response) {
+        self.setState({
+          logo: response.data.attributes.logo,
+          conf_name: response.data.attributes.name,
+          location: response.data.attributes.location,
+          starttime: response.data.attributes.starttime,
+          endtime: response.data.attributes.endtime,
+          other: response.data.attributes.other,
+          twitter: response.data.attributes.twitter,
+          facebook: response.data.attributes.facebook,
+        });
+      },
     });
   }
 
-  componentWillUnmount() {
-    this.serverRequest.abort();
-  }
-
-
   render() {
-    const {logo, confName, location, starttime, endtime, other, twitter, facebook} = this.state;
+    const { logo, conf_name, location, starttime, endtime, other, twitter, facebook } = this.state;
     return (
       <div id="conference-info">
-        <img class="logo" src={logo} />
-        <h3 class="other">{other}</h3>
-        <div class="info-container">
-          <h1>You're at: {confName}</h1>
+        <img className="logo" alt="Logo" src={logo} />
+        <h3 className="other">{other}</h3>
+        <div className="info-container">
+          <h1>You're at: {conf_name}</h1>
           <h2> The location is: {location}</h2>
-          <h2> It begins: {moment.unix(starttime).format("ddd MMMM Do YYYY hh:mm")}</h2>
-          <h2> and ends: {moment.unix(endtime).format("ddd MMMM Do YYYY hh:mm")}</h2>
-          <a href={twitter}> <img src="https://abs.twimg.com/favicons/favicon.ico" /></a>
-          <a href={facebook}> <img src="https://www.facebook.com/rsrc.php/yl/r/H3nktOa7ZMg.ico" /></a>
+          <h2> It begins: {moment.unix(starttime).format('ddd MMMM Do YYYY hh:mm')}</h2>
+          <h2> and ends: {moment.unix(endtime).format('ddd MMMM Do YYYY hh:mm')}</h2>
+          <a href={twitter}> <img alt="Twitter" src="https://abs.twimg.com/favicons/favicon.ico" /></a>
+          <a href={facebook}> <img alt="Facebook" src="https://www.facebook.com/rsrc.php/yl/r/H3nktOa7ZMg.ico" /></a>
         </div>
       </div>
     );
