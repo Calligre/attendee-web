640ea82bfda819fbb3670ff100d936fb4cd3509e
Clarisse Schneider <cmschnei@uwaterloo.ca>
2017-03-22 01:58:24 -0400

    Add card editing to organizer

--- src/js/pages/organizer/Cards.jsx
+++ src/js/pages/organizer/Cards.jsx
@@ -1,9 +1,249 @@
 import React from "react";
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import BrandStore from "stores/BrandStore";
 
 export default class Cards extends React.Component {
+ 
+  constructor() {
+    super();
+    this.state = {
+      cards: [],
+      locations: [],
+      contacts: [],
+      sponsors: [],
+    };
+
+    BrandStore.getAll();
+  }
+
+  componentWillMount() {
+    BrandStore.on("receivedCards", this.getCards);
+    BrandStore.on("receivedContacts", this.getContacts);
+    BrandStore.on("receivedLocations", this.getLocations);
+    BrandStore.on("receivedSponsors", this.getSponsors);
+    BrandStore.on("error", this.showError);
+  }
+
+  componentWillUnmount() {
+    BrandStore.removeListener("receivedCards", this.getCards);
+    BrandStore.removeListener("receivedContacts", this.getContacts);
+    BrandStore.removeListener("receivedLocations", this.getLocations);
+    BrandStore.removeListener("receivedSponsors", this.getSponsors);
+    BrandStore.removeListener("error", this.showError);
+  }
+
+  showError = () => {
+    console.log(BrandStore.error)
+  }
+ 
+  generateCard = (row) => {
+    var updatedCard = {
+      id: -1,
+      data: "",
+    }
+    for (const prop in row) {
+      updatedCard[prop] = row[prop];
+    }
+    return updatedCard;
+  }
+
+  generateLocation = (row) => {
+    var updatedLocation = {
+      id: -1,
+      name: "",
+      address: "",
+    }
+    for (const prop in row) {
+      updatedLocation[prop] = row[prop];
+    }
+    return updatedLocation;
+  }
+
+  generateContact = (row) => {
+    var updatedLocation = {
+      id: -1,
+      name: "",
+      phone: "",
+    }
+    for (const prop in row) {
+      updatedLocation[prop] = row[prop];
+    }
+    return updatedLocation;
+  }
+
+  generateSponsor = (row) => {
+    var updatedLocation = {
+      id: -1,
+      level: "",
+      logo: "",
+      name: "",
+      website: "",
+    }
+    for (const prop in row) {
+      updatedLocation[prop] = row[prop];
+    }
+    return updatedLocation;
+  }
+
+  updateCard = (row, cellName, cellValue) => {
+    BrandStore.updateCard(this.generateCard(row));
+  }
+
+  addCard = (row) => {
+    BrandStore.addCard(this.generateCard(row));
+  }
+
+  deleteCards = (rowKeys) => {
+    for (const key in rowKeys) {
+      BrandStore.deleteCard(rowKeys[key])
+    }
+  }
+
+  getCards = () => {
+    this.setState({
+      cards: BrandStore.cards,
+    });
+  }
+
+  updateContact = (row, cellName, cellValue) => {
+    BrandStore.updateContact(this.generateContact(row));
+  }
+
+  addContact = (row) => {
+    BrandStore.addContact(this.generateContact(row));
+  }
+
+  deleteContacts = (rowKeys) => {
+    for (const key in rowKeys) {
+      BrandStore.deleteContact(rowKeys[key])
+    }
+  }
+ 
+  getContacts = () => {
+    this.setState({
+      contacts: BrandStore.contacts,
+    });
+  }
+
+  updateLocation = (row, cellName, cellValue) => {
+    BrandStore.updateLocation(this.generateLocation(row));
+  }
+
+  addLocation = (row) => {
+    BrandStore.addLocation(this.generateLocation(row));
+  }
+
+  deleteLocations = (rowKeys) => {
+    for (const key in rowKeys) {
+      BrandStore.deleteLocation(rowKeys[key])
+    }
+  }
+
+  getLocations = () => {
+    this.setState({
+      locations: BrandStore.locations,
+    });
+  }
+
+  updateSponsor = (row, cellName, cellValue) => {
+    BrandStore.updateSponsor(this.generateSponsor(row));
+  }
+
+  addSponsor = (row) => {
+    BrandStore.addSponsor(this.generateSponsor(row));
+  }
+
+  deleteSponsors = (rowKeys) => {
+    for (const key in rowKeys) {
+      BrandStore.deleteSponsor(rowKeys[key])
+    }
+  }
+
+  getSponsors = () => {
+    this.setState({
+      sponsors: BrandStore.sponsors,
+    });
+  }
 
   render() {
-  	return (<div> Cards </div>);
+    const selectRowProp = {
+      mode: 'checkbox'
+    };
+    const cellEditPropCards = {
+      mode: 'click',
+      afterSaveCell: this.updateCard,
+    };
+    const optionsCards = {
+      afterInsertRow: this.addCard,
+      afterDeleteRow: this.deleteCards,
+    };
+    const cellEditPropContacts = {
+      mode: 'click',
+      afterSaveCell: this.updateContact,
+    };
+    const optionsContacts = {
+      afterInsertRow: this.addContact,
+      afterDeleteRow: this.deleteContacts,
+    };
+    const cellEditPropLocations = {
+      mode: 'click',
+      afterSaveCell: this.updateLocation,
+    };
+    const optionsLocations = {
+      afterInsertRow: this.addLocation,
+      afterDeleteRow: this.deleteLocations,
+    };
+    const cellEditPropSponsors = {
+      mode: 'click',
+      afterSaveCell: this.updateSponsor,
+    };
+    const optionsSponsors = {
+      afterInsertRow: this.addSponsor,
+      afterDeleteRow: this.deleteSponsors,
+    };
+
+    return (
+      <div>
+        <h1 className="primaryText largeTopMargin">Important Locations</h1>
+        <BootstrapTable data={this.state.locations} striped hover insertRow deleteRow selectRow={selectRowProp} cellEdit={cellEditPropLocations} options={optionsLocations}>
+          <TableHeaderColumn dataField='id' isKey hidden hiddenOnInsert autoValue>Location ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='name' dataSort filter={ { type: 'TextFilter', delay: 100 } }>
+            Location
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='address'>Address</TableHeaderColumn>
+        </BootstrapTable>
+
+        <h1 className="primaryText largeTopMargin">Important Contacts</h1>
+        <BootstrapTable data={this.state.contacts} striped hover insertRow deleteRow selectRow={selectRowProp} cellEdit={cellEditPropContacts} options={optionsContacts}>
+          <TableHeaderColumn dataField='id' isKey hidden hiddenOnInsert autoValue>Contact ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='name' dataSort filter={ { type: 'TextFilter', delay: 100 } }>
+            Contact Name
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='phone'>Phone</TableHeaderColumn>
+        </BootstrapTable>
+
+        <h1 className="primaryText largeTopMargin">Text Cards</h1>
+        <BootstrapTable data={this.state.cards} striped hover insertRow deleteRow selectRow={selectRowProp} cellEdit={cellEditPropCards} options={optionsCards}>
+          <TableHeaderColumn dataField='id' isKey hidden hiddenOnInsert autoValue>Card ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='data' dataSort filter={ { type: 'TextFilter', delay: 100 } }>
+            Text
+          </TableHeaderColumn>
+        </BootstrapTable>
+
+        <h1 className="primaryText largeTopMargin">Sponsors</h1>
+        <BootstrapTable data={this.state.sponsors} striped hover insertRow deleteRow selectRow={selectRowProp} cellEdit={cellEditPropSponsors} options={optionsSponsors}>
+          <TableHeaderColumn dataField='id' isKey hidden hiddenOnInsert autoValue>Sponsor ID</TableHeaderColumn>
+          <TableHeaderColumn dataField='name' dataSort filter={ { type: 'TextFilter', delay: 100 } }>
+            Name
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='level' dataSort filter={ { type: 'TextFilter', delay: 100 } }>
+            Level
+          </TableHeaderColumn>
+          <TableHeaderColumn dataField='logo'>Logo</TableHeaderColumn>
+          <TableHeaderColumn dataField='website'>Website</TableHeaderColumn>
+        </BootstrapTable>
+      </div>
+    );
   }
 
 }
--- src/js/stores/BrandStore.jsx
+++ src/js/stores/BrandStore.jsx
@@ -13,11 +13,19 @@ class BrandStore extends EventEmitter {
     this.contacts = [];
     this.locations = [];
     this.cards = [];
+    this.sponsors = [];
     this.error = null;
   }
 
+  getAll = () => {
+    this.getBranding();
+    this.getCards();
+    this.getLocations();
+    this.getContacts();
+    this.getSponsors();
+  }
 
-  getBranding() {
+  getBranding = () => {
     $.ajax({
       url: `${url}/api/info`,
       dataType: 'json',
@@ -36,7 +44,7 @@ class BrandStore extends EventEmitter {
     });
   }
   
-  getCards() {
+  getCards = () => {
     $.ajax({
       url: `${url}/api/info/card`,
       dataType: 'json',
@@ -54,8 +62,71 @@ class BrandStore extends EventEmitter {
       },
     });
   }
+ 
+  updateCard(card){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/card/" + card.id,
+      data : JSON.stringify(card),
+      type : 'PATCH',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.cards;
+  };
 
-  getContacts() {
+  addCard(card){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/card",
+      data : JSON.stringify(card),
+      type : 'POST',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.cards;
+  };
+
+  deleteCard(id){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/card/" + id,
+      type : 'DELETE',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.cards;
+  };
+
+
+  getContacts = () => {
     $.ajax({
       url: `${url}/api/info/contact`,
       dataType: 'json',
@@ -73,8 +144,70 @@ class BrandStore extends EventEmitter {
       },
     });
   }
+ 
+  updateContact(contact){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/contact/" + contact.id,
+      data : JSON.stringify(contact),
+      type : 'PATCH',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.events;
+  };
+ 
+  addContact(contact){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/contact",
+      data : JSON.stringify(contact),
+      type : 'POST',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.contacts;
+  };
 
-  getLocations() {
+  deleteContact(id){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/contact/" + id,
+      type : 'DELETE',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.contacts;
+  };
+
+  getLocations = () => {
     $.ajax({
       url: `${url}/api/info/location`,
       dataType: 'json',
@@ -92,6 +225,149 @@ class BrandStore extends EventEmitter {
       },
     });
   }
+  
+  updateLocation(location){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/location/" + location.id,
+      data : JSON.stringify(location),
+      type : 'PATCH',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.events;
+  };
+  
+  addLocation(location){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/location",
+      data : JSON.stringify(location),
+      type : 'POST',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.locations;
+  };
+
+  deleteLocation(id){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/location/" + id,
+      type : 'DELETE',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.locations;
+  };
+
+  getSponsors = () => {
+    $.ajax({
+      url: `${url}/api/info/sponsor`,
+      dataType: 'json',
+      type: 'GET',
+      headers: {
+        Authorization: `Bearer ${AuthService.getToken()}`,
+      },
+
+      cache: false,
+      success(response) {
+        dispatcher.dispatch({ type: 'SPONSOR_GET', sponsors: response.data });
+      },
+      error(error) {
+        console.log(`${error.status}: ${error.statusText}`);
+      },
+    });
+  }
+  
+  updateSponsor(sponsor){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/sponsor/" + sponsor.id,
+      data : JSON.stringify(sponsor),
+      type : 'PATCH',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.events;
+  };
+  
+  addSponsor(sponsor){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/sponsor",
+      data : JSON.stringify(sponsor),
+      type : 'POST',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.sponsors;
+  };
+
+  deleteSponsor(id){
+    $.ajax({
+      url: "https://dev.calligre.com/api/info/sponsor/" + id,
+      type : 'DELETE',
+      contentType : 'application/json',
+      processData: false,
+      dataType: 'json',
+      headers: {
+        "Authorization": "Bearer " + AuthService.getToken()
+      },
+      success: function(response){
+        console.log(response);
+      },
+      error: function(error){
+        console.error(error);
+      }
+    });
+    return this.sponsors;
+  };
 
   saveBranding(data) {
     $.ajax({
@@ -136,9 +412,16 @@ class BrandStore extends EventEmitter {
       }
       case 'CARD_GET': {
         this.cards = action.cards.map((card) => {
-          return card.attributes.data;
+          return card.attributes;
         });
         this.emit('receivedCards');
+        break;
+      }
+      case 'SPONSOR_GET': {
+        this.sponsors = action.sponsors.map((sponsor) => {
+          return sponsor.attributes;
+        });
+        this.emit('receivedSponsors');
         break;
       }
       case 'BRANDING_SAVED': {
