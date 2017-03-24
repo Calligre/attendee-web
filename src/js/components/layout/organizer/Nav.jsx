256221ad3b8c8fea262648133259b707ef13417c
Lara Janecka <numbersandlettersandallthat@gmail.com>
2017-03-24 13:17:39 -0400

    linting

--- src/js/components/layout/organizer/Nav.jsx
+++ src/js/components/layout/organizer/Nav.jsx
@@ -1,58 +1,58 @@
-import React from "react";
-import { IndexLink, Link } from "react-router";
+import React from 'react';
+import { IndexLink, Link } from 'react-router';
 
-import AuthService from "util/AuthService";
+import AuthService from 'util/AuthService';
 
 export default class Nav extends React.Component {
   constructor() {
-    super()
+    super();
     this.state = {
       collapsed: true,
     };
   }
 
-  toggleCollapse() {
+  toggleCollapse = () => {
     const collapsed = !this.state.collapsed;
-    this.setState({collapsed});
+    this.setState({ collapsed });
   }
 
   render() {
     const { location } = this.props;
     const { collapsed } = this.state;
-    const brandingClass = location.pathname === "/" ? "active" : "";
-    const calendarClass = location.pathname.match(/^\/calendar/) ? "active" : "";
-    const preferencesClass = location.pathname.match(/^\/preferences/) ? "active" : "";
-    const surveyClass = location.pathname.match(/^\/surveys/) ? "active" : "";
-    const notificationClass = location.pathname.match(/^\/notifications/) ? "active" : "";
-    const navClass = collapsed ? "collapse" : "";
+    const brandingClass = location.pathname === '/' ? 'active' : '';
+    const calendarClass = location.pathname.match(/^\/calendar/) ? 'active' : '';
+    const preferencesClass = location.pathname.match(/^\/preferences/) ? 'active' : '';
+    const surveyClass = location.pathname.match(/^\/surveys/) ? 'active' : '';
+    const notificationClass = location.pathname.match(/^\/notifications/) ? 'active' : '';
+    const navClass = collapsed ? 'collapse' : '';
 
     return (
-      <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" onClick={this.toggleCollapse.bind(this)} >
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
+      <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle" onClick={this.toggleCollapse} >
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar" />
+              <span className="icon-bar" />
+              <span className="icon-bar" />
             </button>
           </div>
-          <div class={"navbar-collapse " + navClass} id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <li class={brandingClass}>
-                <IndexLink to="/" onClick={this.toggleCollapse.bind(this)}>Branding</IndexLink>
+          <div className={`navbar-collapse ${navClass}`} id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li className={brandingClass}>
+                <IndexLink to="/" onClick={this.toggleCollapse}>Branding</IndexLink>
               </li>
-              <li class={calendarClass}>
-                <Link to="calendar" onClick={this.toggleCollapse.bind(this)}>Calendar</Link>
+              <li className={calendarClass}>
+                <Link to="calendar" onClick={this.toggleCollapse}>Calendar</Link>
               </li>
-              <li class={preferencesClass}>
-                <Link to="preferences" onClick={this.toggleCollapse.bind(this)}>Preferences</Link>
+              <li className={preferencesClass}>
+                <Link to="preferences" onClick={this.toggleCollapse}>Preferences</Link>
               </li>
-              <li class={surveyClass}>
-                <Link to="surveys" onClick={this.toggleCollapse.bind(this)}>Surveys</Link>
+              <li className={surveyClass}>
+                <Link to="surveys" onClick={this.toggleCollapse}>Surveys</Link>
               </li>
-              <li class={notificationClass}>
-                <Link to="notifications" onClick={this.toggleCollapse.bind(this)}>Notifications</Link>
+              <li className={notificationClass}>
+                <Link to="notifications" onClick={this.toggleCollapse}>Notifications</Link>
               </li>
               <li>
                 <Link to="login" onClick={AuthService.logout}>Logout</Link>
