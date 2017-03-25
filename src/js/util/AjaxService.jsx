691fd8be97b560e4ddb1015e5778f175bd0cca4f
Lara Janecka <numbersandlettersandallthat@gmail.com>
2017-03-24 23:43:51 -0400

    docs

--- src/js/util/AjaxService.jsx
+++ src/js/util/AjaxService.jsx
@@ -10,6 +10,12 @@ class AjaxService {
     return this.url;
   }
 
+  /*
+    Makes a get call
+      param: endpoint - hits api/endpoint
+      param: success - call back for status 2xx
+      param: error - call back for all non successful statuses
+  */
   get(options) {
     const { endpoint, success, error } = options;
     const xhr = new XMLHttpRequest();
@@ -28,6 +34,13 @@ class AjaxService {
     xhr.send();
   }
 
+  /*
+    Makes a patch call
+      param: endpoint - hits api/endpoint/data.id
+      param: data - values to update to
+      param: success - call back for status 2xx
+      param: error - call back for all non successful statuses
+  */
   update(options) {
     const { endpoint, success, error, data } = options;
     const xhr = new XMLHttpRequest();
@@ -47,6 +60,13 @@ class AjaxService {
     xhr.send(JSON.stringify(data));
   }
 
+  /*
+    Makes a post call
+      param: endpoint - hits api/endpoint
+      param: data - values to create with
+      param: success - call back for status 2xx
+      param: error - call back for all non successful statuses
+  */
   create(options) {
     const { endpoint, success, error, data } = options;
     const xhr = new XMLHttpRequest();
@@ -66,6 +86,13 @@ class AjaxService {
     xhr.send(JSON.stringify(data));
   }
 
+  /*
+    Makes a delete call
+      param: endpoint - hits api/endpoint/id
+      param: id - id of element to be deleted
+      param: success - call back for status 2xx
+      param: error - call back for all non successful statuses
+  */
   delete(options) {
     const { endpoint, success, error, id } = options;
     const xhr = new XMLHttpRequest();
@@ -84,8 +111,17 @@ class AjaxService {
     };
     xhr.send();
   }
-
-
+  /*
+    Makes a generic call
+      param: url - hits url
+      param: endpoint - hits api/endpoint/id
+      param: type - type of the call being made (HTTP verb)
+      param: data - params being passed with call
+      param: contentType - passed as Content-Type header
+      param: noAuth - stops passing of Authorization header
+      param: success - call back for status 2xx
+      param: error - call back for all non successful statuses
+  */
   call(options) {
     const { url, endpoint, type, data, contentType, noAuth, success, error } = options;
     const xhr = new XMLHttpRequest();
