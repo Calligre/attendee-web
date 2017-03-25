fc0240971e2a2b37bddc6185e9d44c70f1719169
Clarisse Schneider <cmschnei@uwaterloo.ca>
2017-03-24 20:21:07 -0400

    style conference switching

--- src/js/pages/attendee/Layout.jsx
+++ src/js/pages/attendee/Layout.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Sidebar, SidebarItem } from 'react-responsive-sidebar';
+import Modal from 'react-modal';
 
 import Footer from 'components/layout/Footer';
 import BrandStore from 'stores/BrandStore';
+import ConferenceStore from 'stores/ConferenceStore';
+import PreferenceStore from 'stores/PreferenceStore';
 import AuthService from 'util/AuthService';
 
 export default class Layout extends React.Component {
@@ -10,10 +13,38 @@ export default class Layout extends React.Component {
     super(props);
     this.state = {
       branding: {},
+      switcherModal: false,
+      newsfeed: PreferenceStore.getDefaults().newsfeed,
+      conferences: [],
     };
 
-    BrandStore.on('receivedBranding', this.setBranding);
     BrandStore.getBranding();
+    PreferenceStore.loadAll();
+    ConferenceStore.getConferences();
+  }
+
+  componentWillMount() {
+    PreferenceStore.on('loaded', this.loadPreferences);
+    PreferenceStore.on('error', this.showError);
+    ConferenceStore.on('loadedConferences', this.loadConferences);
+    ConferenceStore.on('error', this.showError);
+    BrandStore.on('receivedBranding', this.setBranding);
+    BrandStore.on('error', this.showError);
+  }
+
+  componentWillUnmount() {
+    PreferenceStore.removeListener('loaded', this.loadPreferences);
+    PreferenceStore.removeListener('error', this.showError);
+    ConferenceStore.removeListener('loadedConferences', this.loadedConferences);
+    ConferenceStore.removeListener('error', this.showError);
+  }
+
+  loadPreferences = () => {
+    this.setState({ newsfeed: PreferenceStore.preferences.newsfeed });
+  }
+
+  loadConferences = () => {
+    this.setState({ conferences: ConferenceStore.conferences });
   }
 
   setBranding = () => {
@@ -27,8 +58,23 @@ export default class Layout extends React.Component {
     myStyle.insertRule(".nameContainer { background-image: url('" + branding.background_logo + "')!important }", 0);
   }
 
+  showError = () => {
+    console.error(PreferenceStore.error);
+    console.error(ConferenceStore.error);
+    console.error(BrandStore.error);
+  }
+
+  showConferenceSwitcher = () => {
+    this.setState({ switcherModal: true });
+  }
+
+  closeModal = () => {
+    this.setState({ switcherModal: false });
+  }
+
   render() {
-    const { branding } = this.state;
+    const { branding, newsfeed, switcherModal } = this.state;
+    var self = this;
     
     if (!branding) {
       return null;
@@ -42,12 +88,21 @@ export default class Layout extends React.Component {
 
     const items = [
       <SidebarItem href='/'>Home</SidebarItem>,
-      <SidebarItem href='/newsfeed'>News Feed</SidebarItem>,
+    ];
+    if (newsfeed) {
+      items.push(<SidebarItem href='/newsfeed'>News Feed</SidebarItem>);
+    }
+    Array.prototype.push.apply(items, [
       <SidebarItem href='/events'>Events</SidebarItem>,
       <SidebarItem href='/people'>People</SidebarItem>,
       <SidebarItem href='/profile'>My Profile</SidebarItem>,
-      <SidebarItem href='/login' onClick={AuthService.logout}>Logout</SidebarItem>,
-    ];
+      <SidebarItem>
+        <a className="sidebarLink" onClick={this.showConferenceSwitcher}>Switch Conferences</a>
+      </SidebarItem>,
+      <SidebarItem href='/login'>
+        <a className="sidebarLink" onClick={AuthService.logout}>Logout</a>
+      </SidebarItem>,
+    ]);
 
     return (
       <div>
@@ -61,7 +116,29 @@ export default class Layout extends React.Component {
             <Footer/>
           </div>
         </Sidebar>
+		<Modal isOpen={switcherModal} onRequestClose={this.closeModal} contentLabel="Switch Conferences">
+            <div className="conferenceSwitcher">
+              <h1 className="primaryText">Switch Conference</h1>
+              {this.state.conferences.map(conference =>
+                  <Conference key={conference.id} {...conference} />
+              )}
+            </div>
+            <button className="secondaryBackground" onClick={this.closeModal}>Cancel</button>
+          </Modal>
       </div>
     );
   }
 }
+
+const Conference = React.createClass({
+  render() {
+    return (
+      <div className="conferenceElement">
+        <a href={`https://${this.props.url}`} >
+          <img src={this.props.logo} />
+          <h3>{this.props.name}</h3>
+        </a>
+      </div>
+    );
+  },
+});
