889a816e3ee4ee99f1b2d01d272e56111c4d6ae8
Steven Locke <s3locke@uwaterloo.ca>
2017-03-27 18:26:50 -0400

    Linting

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -18,35 +18,35 @@ export default class Card extends React.Component {
     const { item } = this.props;
     const { type } = this.state;
 
-    let buttonStyle = this.props.buttonStyle || {};
-    let headerStyle = this.props.headerStyle || {};
+    const buttonStyle = this.props.buttonStyle || {};
+    const headerStyle = this.props.headerStyle || {};
 
     switch (type) {
       case 'event':
         renderedContent = <Event key={item.id} {...item} />;
         break;
       case 'content':
-        renderedContent = <ContentCard data={item}/>;
+        renderedContent = <ContentCard data={item} />;
         break;
       case 'sponsor':
-        renderedContent = <SponsorCard data={item} headerStyle={headerStyle}/>;
+        renderedContent = <SponsorCard data={item} headerStyle={headerStyle} />;
         break;
       case 'contact':
-        renderedContent = <ContactCard data={item} headerStyle={headerStyle}/>;
+        renderedContent = <ContactCard data={item} headerStyle={headerStyle} />;
         break;
       case 'location':
-        renderedContent = <LocationCard data={item} headerStyle={headerStyle} buttonStyle={buttonStyle}/>;
+        renderedContent = <LocationCard data={item} headerStyle={headerStyle} buttonStyle={buttonStyle} />;
         break;
       case 'download':
-        renderedContent = <DownloadCard data={item} headerStyle={headerStyle} buttonStyle={buttonStyle}/>;
+        renderedContent = <DownloadCard data={item} headerStyle={headerStyle} buttonStyle={buttonStyle} />;
         break;
       case 'social':
-        let facebook = this.props.facebook || null;
-        let twitter = this.props.twitter || null;
-        renderedContent = <SocialCard facebook={facebook} twitter={twitter} headerStyle={headerStyle}/>;
+        const facebook = this.props.facebook || null;
+        const twitter = this.props.twitter || null;
+        renderedContent = <SocialCard facebook={facebook} twitter={twitter} headerStyle={headerStyle} />;
         break;
       case 'survey':
-        renderedContent = <SurveyCard data={item} headerStyle={headerStyle} buttonStyle={buttonStyle}/>;
+        renderedContent = <SurveyCard data={item} headerStyle={headerStyle} buttonStyle={buttonStyle} />;
         break;
       default:
         return null;
@@ -57,158 +57,170 @@ export default class Card extends React.Component {
         {renderedContent}
       </div>
     );
-  }; 
+  }
 }
 
-var ContentCard = React.createClass({
-  render: function() {
+const ContentCard = React.createClass({
+  render() {
     return (
-	  <BetterCard>
-		<CardTitle
-		  avatar="https://image.flaticon.com/icons/svg/172/172116.svg"
-		  title="Message"/>
+      <BetterCard>
+        <CardTitle
+          avatar="https://image.flaticon.com/icons/svg/172/172116.svg"
+          title="Message"
+        />
         <CardText>{this.props.data}</CardText>
-	  </BetterCard>
+      </BetterCard>
     );
-  }
+  },
 });
 
-var ContactCard = React.createClass({
-  render: function() {
-    var contactNodes = this.props.data.map(function(contact) {
-      return (
-        <div className="contact" key={contact.id}>
-          <h4>{contact.name}</h4>
-          <p>{contact.phone}</p>
-        </div>
-      );
-    });
+const ContactCard = React.createClass({
+  render() {
+    const contactNodes = this.props.data.map(contact => (
+      <div className="contact" key={contact.id}>
+        <h4>{contact.name}</h4>
+        <p>{contact.phone}</p>
+      </div>
+      ));
     return (
-	  <BetterCard>
-		<CardTitle
-		  avatar="https://image.flaticon.com/icons/svg/172/172166.svg"
-		  title="Information"/>
-		<CardTitle
-		  title="Important Contacts"
-          style={this.props.headerStyle}/>
+      <BetterCard>
+        <CardTitle
+          avatar="https://image.flaticon.com/icons/svg/172/172166.svg"
+          title="Information"
+        />
+        <CardTitle
+          title="Important Contacts"
+          style={this.props.headerStyle}
+        />
         <CardText>{contactNodes}</CardText>
-	  </BetterCard>
+      </BetterCard>
     );
-  }
+  },
 });
 
-var LocationCard = React.createClass({
-  render: function() {
-    var locationNodes = this.props.data.map(function(confLocation) {
-      return (
-        <div className="location" key={confLocation.id}>
-          <h4>{confLocation.name}</h4>
-          <p>{confLocation.address}</p>
-        </div>
-      );
-    });
+const LocationCard = React.createClass({
+  render() {
+    const locationNodes = this.props.data.map(confLocation => (
+      <div className="location" key={confLocation.id}>
+        <h4>{confLocation.name}</h4>
+        <p>{confLocation.address}</p>
+      </div>
+      ));
     return (
-	  <BetterCard>
-		<CardTitle
-		  avatar="https://image.flaticon.com/icons/svg/172/172166.svg"
-		  title="Information"/>
-		<CardTitle
-		  title="Locations"
-          style={this.props.headerStyle}/>
+      <BetterCard>
+        <CardTitle
+          avatar="https://image.flaticon.com/icons/svg/172/172166.svg"
+          title="Information"
+        />
+        <CardTitle
+          title="Locations"
+          style={this.props.headerStyle}
+        />
         <CardText>{locationNodes}</CardText>
-	  </BetterCard>
+      </BetterCard>
     );
-  }
+  },
 });
 
-var DownloadCard = React.createClass({
-  render: function() {
+const DownloadCard = React.createClass({
+  render() {
     return (
-	  <BetterCard>
-		<CardTitle
-		  avatar="https://image.flaticon.com/icons/svg/172/172140.svg"
-		  title="Download"/>
-        { this.props.data.name === "Map" &&
-          <CardMedia
-            aspectRatio="wide"
-            image="https://images.pexels.com/photos/297642/pexels-photo-297642.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"/> }
-        { this.props.data.name === "Conference Package" &&
-          <CardMedia
-            aspectRatio="wide"
-            image="https://images.pexels.com/photos/30027/pexels-photo-30027.jpg?w=940&h=650&auto=compress&cs=tinysrgb"/> }
-		<CardTitle
-		  title={this.props.data.name}
-          style={this.props.headerStyle}/>
-		<CardActions>
-		  <Button style={this.props.buttonStyle} label="Download" href={this.props.data.link} download/>
-		</CardActions>
-	  </BetterCard>
-    );
-  }
-});
-
-var SocialCard = React.createClass({
-  render: function() {
-    return (
-	  <BetterCard>
-		<CardTitle
-		  avatar="https://image.flaticon.com/icons/svg/172/172115.svg"
-		  title="Connect With Us"/>
+      <BetterCard>
+        <CardTitle
+          avatar="https://image.flaticon.com/icons/svg/172/172140.svg"
+          title="Download"
+        />
+        { this.props.data.name === 'Map' &&
         <CardMedia
           aspectRatio="wide"
-          image="https://images.pexels.com/photos/267447/pexels-photo-267447.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"/>
+          image="https://images.pexels.com/photos/297642/pexels-photo-297642.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
+        /> }
+        { this.props.data.name === 'Conference Package' &&
+        <CardMedia
+          aspectRatio="wide"
+          image="https://images.pexels.com/photos/30027/pexels-photo-30027.jpg?w=940&h=650&auto=compress&cs=tinysrgb"
+        /> }
+        <CardTitle
+          title={this.props.data.name}
+          style={this.props.headerStyle}
+        />
+        <CardActions>
+          <Button style={this.props.buttonStyle} label="Download" href={this.props.data.link} download />
+        </CardActions>
+      </BetterCard>
+    );
+  },
+});
+
+const SocialCard = React.createClass({
+  render() {
+    return (
+      <BetterCard>
+        <CardTitle
+          avatar="https://image.flaticon.com/icons/svg/172/172115.svg"
+          title="Connect With Us"
+        />
+        <CardMedia
+          aspectRatio="wide"
+          image="https://images.pexels.com/photos/267447/pexels-photo-267447.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
+        />
         <CardText>
           { this.props.facebook && <SocialIcon url={this.props.facebook} className="socialLink" /> }
           { this.props.twitter && <SocialIcon url={this.props.twitter} className="socialLink" /> }
         </CardText>
-	  </BetterCard>
+      </BetterCard>
     );
-  }
+  },
 });
 
-var SurveyCard = React.createClass({
-  render: function() {
+const SurveyCard = React.createClass({
+  render() {
     return (
-	  <BetterCard>
-		<CardTitle
-		  avatar="https://image.flaticon.com/icons/svg/172/172168.svg"
-		  title="Survey"/>
+      <BetterCard>
+        <CardTitle
+          avatar="https://image.flaticon.com/icons/svg/172/172168.svg"
+          title="Survey"
+        />
         <CardMedia
           aspectRatio="wide"
-          image="https://images.pexels.com/photos/210585/pexels-photo-210585.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"/>
-		<CardTitle
-		  title={this.props.data.name}
-          style={this.props.headerStyle}/>
-		<CardText>{this.props.data.description}</CardText>
-		<CardActions>
-		  <Button style={this.props.buttonStyle} href={this.props.data.link} target="_blank" label="Take Survey" />
-		</CardActions>
-	  </BetterCard>
+          image="https://images.pexels.com/photos/210585/pexels-photo-210585.jpeg?w=940&h=650&auto=compress&cs=tinysrgb"
+        />
+        <CardTitle
+          title={this.props.data.name}
+          style={this.props.headerStyle}
+        />
+        <CardText>{this.props.data.description}</CardText>
+        <CardActions>
+          <Button style={this.props.buttonStyle} href={this.props.data.link} target="_blank" label="Take Survey" />
+        </CardActions>
+      </BetterCard>
     );
-  }
+  },
 });
 
-var SponsorCard = React.createClass({
-  render: function() {
-    let sponsors = this.props.data.map(sponsor =>
-        <div className="sponsorContainer">
-          <a href={sponsor.website}>
-            <img src={sponsor.logo}/>
-          </a>
-          <h3>{sponsor.name}</h3>
-        </div>
+const SponsorCard = React.createClass({
+  render() {
+    const sponsors = this.props.data.map(sponsor =>
+      <div className="sponsorContainer">
+        <a href={sponsor.website}>
+          <img src={sponsor.logo} />
+        </a>
+        <h3>{sponsor.name}</h3>
+      </div>,
     );
     return (
-	  <BetterCard>
-		<CardTitle
-		  avatar="https://image.flaticon.com/icons/svg/172/172096.svg"
-		  title="Sponsors"/>
-		<CardTitle
-		  title={this.props.data[0].level + " Sponsors"}
-          style={this.props.headerStyle}/>
+      <BetterCard>
+        <CardTitle
+          avatar="https://image.flaticon.com/icons/svg/172/172096.svg"
+          title="Sponsors"
+        />
+        <CardTitle
+          title={`${this.props.data[0].level} Sponsors`}
+          style={this.props.headerStyle}
+        />
         <CardText>{sponsors}</CardText>
-	  </BetterCard>
+      </BetterCard>
     );
-  }
+  },
 });
 
