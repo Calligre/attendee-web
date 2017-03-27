360081ada7c69eba57c0c0357c137f1fa4c45875
Steven Locke <s3locke@uwaterloo.ca>
2017-03-26 20:58:22 -0400

    Show the conference title on the loading page

--- src/js/pages/Login.jsx
+++ src/js/pages/Login.jsx
@@ -1,21 +1,55 @@
-import React, { PropTypes } from 'react'
-import AuthService from 'util/AuthService'
+import React, { PropTypes } from 'react';
+import AuthService from 'util/AuthService';
+import BrandStore from 'stores/BrandStore';
 
 export default class Login extends React.Component {
   static contextTypes = {
-    router: PropTypes.object
+    router: PropTypes.object,
   }
 
   static propTypes = {
     location: PropTypes.object,
   }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      branding: null,
+    };
+
+    BrandStore.getBranding();
+  }
+
+  componentWillMount() {
+    BrandStore.on('receivedBranding', this.setBranding);
+    BrandStore.on('error', this.showError);
+  }
+
+  componentWillUnmount() {
+    BrandStore.removeListener('receivedBranding', this.setBranding);
+    BrandStore.removeListener('error', this.showError);
+  }
+
+  setBranding = () => {
+    this.setState({
+      branding: BrandStore.branding,
+    });
+  }
+
+  showError = () => {
+    console.log(BrandStore.error);
+  }
+
   render() {
-    if (localStorage.getItem('logging_in')) {
+    const { branding } = this.state;
+    
+    if (localStorage.getItem('logging_in') || !branding) {
       return (<div></div>)
     }
 
     return (
       <div id="loginContainer">
+        <div> Welcome to: {branding.name} </div>
         <div className="logo">
           <div className="logoC primaryBackground"></div>
           <div className="rest">alligre</div>
