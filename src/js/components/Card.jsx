5e9f64fab0f72d584254141996fe401939423a4c
Steven Locke <s3locke@uwaterloo.ca>
2017-03-27 18:20:53 -0400

    Added href to button
    Linking it to the survey. Opens in sane windows though, maybe that should be changed later.

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -181,7 +181,7 @@ var SurveyCard = React.createClass({
           style={this.props.headerStyle}/>
 		<CardText>{this.props.data.description}</CardText>
 		<CardActions>
-		  <Button style={this.props.buttonStyle} label="Take Survey" />
+		  <Button style={this.props.buttonStyle} href={this.props.data.link} label="Take Survey" />
 		</CardActions>
 	  </BetterCard>
     );
